a1547bef8652e6d67172e96c1818c7e73b0e138f
GUI-NOTEBOOK\Guilherme Abacherli <guilherme.abacherli@fatec.sp.gov.br>
2018-11-13 19:06:14 -0200

    Criados os métodos de leitura da base de dados utilizando a VIEW de pedidos pendentes.

--- src/main/java/br/com/pi5s/icomida/dao/PedidoDAO.java
+++ src/main/java/br/com/pi5s/icomida/dao/PedidoDAO.java
@@ -1,6 +1,7 @@
 package br.com.pi5s.icomida.dao;
 
-import br.com.pi5s.icomida.entity.Pedido;
+//import br.com.pi5s.icomida.entity.Pedido;
+import br.com.pi5s.icomida.view.PedidoPendenteCozinha;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -12,36 +13,29 @@ import javax.persistence.Persistence;
  */
 public class PedidoDAO {
 
-    public List<Pedido> listarPedidos() {
+    public List<PedidoPendenteCozinha> listarPedidos() {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("iComidaRESTPU");
         EntityManager em = emf.createEntityManager();
 
         try {
-            List<Pedido> listaPedidos = new ArrayList<>();
+            //List<Pedido> listaPedidos = new ArrayList<>();
+            List<PedidoPendenteCozinha> listaPedidosPendentes = new ArrayList<>();
 
             em.getTransaction().begin();
 
-//            listaPedidos = em.createQuery(""
-//                    + "SELECT NEW br.com.pi5s.icomida.entity.Pedido("
-//                    + "p.pedidoId, "
-//                    + "p.pedidoData, "
-//                    + "p.pedidoSubtotal, "
-//                    + "p.pedidoTaxaGarcom, "
-//                    + "p.pedidoValorTotal, "
-//                    + "p.mesaId, p.funcionarioId, "
-//                    + "p.empresaId, "
-//                    + "p.sttsId) "
-//                    + "FROM Pedido p")
-//                    .getResultList();
-//
-            listaPedidos = em.createQuery(""
-                    + "SELECT p "
-                    + "FROM Pedido p")
-                    .getResultList();
+            //listaPedidos = em.createQuery(""
+            //+ "SELECT p "
+            //+ "FROM Pedido p")
+            //.getResultList();
+            //
+            listaPedidosPendentes
+                    = em.createQuery(""
+                            + "SELECT p FROM PedidoPendenteCozinha AS p")
+                            .getResultList();
 
             em.getTransaction().commit();
-            return listaPedidos;
+            return listaPedidosPendentes;
 
         } catch (Exception e) {
             System.out.println("Erro ao listar pedidos (REST).\nMensagem de erro:\n" + e.getMessage());
