f3d18b28e96d88275a63b8801e6b5046edd552b4
unknown <guilherme.abacherli@fatec.sp.gov.br>
2018-11-19 21:39:43 -0200

    Importado bootstrap no retorno dos pedidos para cozinha. Retornando VIEW com pedidos pendentes da base de dados.

--- src/main/java/br/com/pi5s/icomida/rest/IcomidaResource.java
+++ src/main/java/br/com/pi5s/icomida/rest/IcomidaResource.java
@@ -86,7 +86,17 @@ public class IcomidaResource {
             listaPedidosPendentes.addAll(pedidoDao.listarPedidos());
 
             String pedido_item_html = ""
-                    + "<table>"
+                    + "<!DOCTYPE html>\n"
+                    + "<html>\n"
+                    + "    <head>\n"
+                    + "        <title>iComidaREST</title>\n"
+                    + "        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n"
+                    + "    </head>"
+                    + "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\">"
+                    + "<script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\"></script>\n"
+                    + "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js\" integrity=\"sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49\" crossorigin=\"anonymous\"></script>\n"
+                    + "<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js\" integrity=\"sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy\" crossorigin=\"anonymous\"></script>"
+                    + "<table class='table'>"
                     + "     <tr>"
                     + "         <th>"
                     + "              Pedido ID"
